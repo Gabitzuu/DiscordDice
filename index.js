8ba3b534556a2b76d932aff6b10c63923f207a5a
Liam Townsley <liamtownsley42@gmail.com>
2019-03-11 23:38:27 +0000

    Adding error throwing.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.dice = function(author, maxnumber) {
     let randomnumber = Math.floor(Math.random() * maxnumber) + 1;
-
+    if(!author || !maxnumber) throw Error('Arguments not inputted.');
     var embedroll = new Discord.RichEmbed()
         .setTitle("Dice", author.avatarURL)
         .setDescription('You rolled out: ' + randomnumber)
